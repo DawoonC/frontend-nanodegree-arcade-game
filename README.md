2599a3e3a694198ee150cf4bdcf5c973d22c455b
DawoonC <cdw_3@naver.com>
2014-10-16 21:20:26 +0900

    Add more functionalities   - score   - life   - gameover sign

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-frontend-nanodegree-arcade-game
-===============================
+# DW Frogger
+A classic arcade game on web page for the final project of Object-Oriented JavaScript and Front-End Nanodegree at Udacity.
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+<br>
+Object-Oriented JavaScript course at Udacity: https://www.udacity.com/course/ud015
+<br>
 
-for self-checking their submission.
+### License
+No license information
+
+<br>
+<i>If you are interested in the result of this project, you can find at: http://dw-frogger.appspot.com</i>
+<br><i>This project was built on Google App Engine.</i>
+
+## How to play
+Use arrow key on your keyboard to move the player.<br>
+Be careful of ladybugs and water tiles, you will lose your life if you got hit by them or step on them.<br>
+Collect many gems as possible.<br>
+<br>
+Have fun!
--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 var enemyPosY = [60, 143, 226];
-var enemySpeed = [80, 100, 130, 160, 200, 250, 300];
+var enemySpeed = [100, 130, 160, 200, 250, 300, 400];
+var gemPosX = [0, 101, 202, 303, 404, 505, 606, 707, 808];
+var gemImages = ['images/Gem_Orange.png', 'images/Gem_Blue.png', 'images/Gem_Green.png'];
+var playerImages = [
+  'images/char-boy.png',
+  'images/char-cat-girl.png',
+  'images/char-horn-girl.png',
+  'images/char-pink-girl.png',
+  'images/char-princess-girl.png'
+];
 
 /**
  * Enemies our player must avoid.
  */
 var Enemy = function() {
-    this.sprite = 'images/enemy-bug.png';
-    this.x = -100;
-    this.y = enemyPosY[Math.floor(Math.random()*3)];
-    this.speed = enemySpeed[Math.floor(Math.random()*7)];
+  this.sprite = 'images/enemy-bug.png';
+  this.x = -100;
+  this.y = enemyPosY[Math.floor(Math.random() * 3)];
+  this.speed = enemySpeed[Math.floor(Math.random() * 7)];
 }
 /** 
  * Updates the enemy's position.
@@ -16,40 +25,51 @@ var Enemy = function() {
  * @param dt A time delta between ticks.
  */
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x = this.x+(this.speed*dt);
-    if (this.x > 550) {
-        this.x = -100;
-        this.y = this.y + 83;
-        this.speed = enemySpeed[Math.floor(Math.random()*7)];
-        if (this.y > 226) {
-            this.y = 60;
-        }
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + (this.speed * dt);
+  if (this.x > 960) {
+    this.x = -100;
+    this.y = this.y + 83;
+    this.speed = enemySpeed[Math.floor(Math.random() * 7)];
+    if (this.y > 226) {
+      this.y = 60;
     }
-    
-    if (this.x > -50 && this.x < 50) {
-        this.tileX = 0;
-    } else if (this.x > 50 && this.x < 150) {
-        this.tileX = 101;
-    } else if (this.x > 150 && this.x < 250) {
-        this.tileX = 202;
-    } else if (this.x > 250 && this.x < 350) {
-        this.tileX = 303;
-    } else if (this.x > 350 && this.x < 450) {
-        this.tileX = 404;
-    }
+  }
 
-    if (player.x === this.tileX && player.y === this.y) {
-        player.reset();
-    }
+  if (this.x > -50 && this.x < 50) {
+    this.tileX = 0;
+  } else if (this.x > 50 && this.x < 150) {
+    this.tileX = 101;
+  } else if (this.x > 150 && this.x < 250) {
+    this.tileX = 202;
+  } else if (this.x > 250 && this.x < 350) {
+    this.tileX = 303;
+  } else if (this.x > 350 && this.x < 450) {
+    this.tileX = 404;
+  } else if (this.x > 450 && this.x < 550) {
+    this.tileX = 505;
+  } else if (this.x > 550 && this.x < 650) {
+    this.tileX = 606;
+  } else if (this.x > 650 && this.x < 750) {
+    this.tileX = 707;
+  } else if (this.x > 750 && this.x < 850) {
+    this.tileX = 808;
+  } else if (this.x > 850) {
+    this.tileX = 1;
+  }
+
+  if (player.x === this.tileX && player.y === this.y) {
+    player.reset();
+    gameLife.decrease();
+  }
 }
 /**
  * Renders the enemy on the screen.
  */
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 
@@ -57,8 +77,8 @@ Enemy.prototype.render = function() {
  * Player class
  */
 var Player = function() {
-  this.pImg = 'images/char-boy.png';
-  this.x = 202;
+  this.pImg = playerImages[Math.floor(Math.random() * 5)];
+  this.x = 404;
   this.y = 392;
 }
 /** 
@@ -69,7 +89,7 @@ var Player = function() {
 Player.prototype.update = function() {
   if (this.ctlKey === 'left' && this.x != 0) {
     this.x = this.x - 101;
-  } else if (this.ctlKey === 'right' && this.x != 404) {
+  } else if (this.ctlKey === 'right' && this.x != 808) {
     this.x = this.x + 101;
   } else if (this.ctlKey === 'up') {
     this.y = this.y - 83;
@@ -80,14 +100,15 @@ Player.prototype.update = function() {
 
   if (this.y < 60) {
     this.reset();
+    gameLife.decrease();
   }
 }
 /**
  * Move the player to initial position.
  */
 Player.prototype.reset = function() {
-    this.x = 202;
-    this.y = 392;
+  this.x = 404;
+  this.y = 392;
 }
 /**
  * Renders the player on the screen.
@@ -103,25 +124,115 @@ Player.prototype.handleInput = function(key) {
   this.ctlKey = key;
 }
 
+
+/**
+ * A player should try to collect gems.
+ * Gems appean on the map randomly,
+ * and whenever player collect one, the enemy increments by 1.
+ */
+var Gem = function() {
+  this.gemImg = gemImages[Math.floor(Math.random() * 3)];
+  this.x = gemPosX[Math.floor(Math.random() * 9)];
+  this.y = enemyPosY[Math.floor(Math.random() * 3)];
+}
+/** 
+ * Updates the gem's position.
+ * Gem's position will reset whenever player touches it.
+ */
+Gem.prototype.update = function() {
+  if (player.x === this.x && player.y === this.y) {
+    this.gemImg = gemImages[Math.floor(Math.random() * 3)];
+    this.x = gemPosX[Math.floor(Math.random() * 9)];
+    this.y = enemyPosY[Math.floor(Math.random() * 3)];
+    if (allEnemies.length < 30) {
+      allEnemies.push(new Enemy());
+    }
+    if (gameScore.score < 30) {
+      gameScore.score++;
+    }
+  }
+}
+/**
+ * Renders the gem on the screen.
+ */
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.gemImg), this.x, this.y);
+}
+
+
+/**
+ * Score shows number of gems player has collected.
+ */
+var Score = function() {
+  this.scoreImg = 'images/Gem_Orange_s.png';
+  this.score = 0;
+}
+/**
+ * Renders the score on the screen.
+ */
+Score.prototype.render = function() {
+  var x = 0;
+  for (var i = 0; i < this.score; i++) {
+    ctx.drawImage(Resources.get(this.scoreImg), x, 10);
+    x = x + 30;
+  }
+}
+
+
+/**
+ * Life of the player.
+ */
+var Life = function() {
+  this.lifeImg = 'images/Heart_s.png';
+  this.life = 5;
+}
+/**
+ * Renders the life on the screen.
+ */
+Life.prototype.render = function() {
+  var x = 0;
+  for (var i = 0; i < this.life; i++) {
+    ctx.drawImage(Resources.get(this.lifeImg), x, 590);
+    x = x + 50;
+  }
+  if (this.life === 0) {
+    ctx.drawImage(Resources.get('images/gameover.png'), 0, 50);
+  }
+}
+/**
+ * Decrease number of lives.
+ */
+Life.prototype.decrease = function() {
+  if (this.life > 0) {
+    this.life = this.life - 1;
+  }
+}
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var enemyA = new Enemy();
 var enemyB = new Enemy();
 var enemyC = new Enemy();
-var allEnemies = [enemyA, enemyB, enemyC];
+var enemyD = new Enemy();
+var allEnemies = [enemyA, enemyB, enemyC, enemyD];
 
 var player = new Player();
+var gem = new Gem();
+var gameScore = new Score();
+var gameLife = new Life();
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
