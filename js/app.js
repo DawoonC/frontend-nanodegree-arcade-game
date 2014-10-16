aecf73c7293cd6398d4c39c8cf817d1dcff777cd
DawoonC <cdw_3@naver.com>
2014-10-17 00:09:47 +0900

    add additional event listener to disable default arrow key scroll actions.

--- js/app.js
+++ js/app.js
@@ -233,6 +233,11 @@ document.addEventListener('keyup', function(e) {
     39: 'right',
     40: 'down'
   };
-
   player.handleInput(allowedKeys[e.keyCode]);
 });
+// This listens for key presses and disables default scroll actions.
+document.addEventListener('keydown', function(e) {
+  if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    e.preventDefault();
+  }  
+}, false);
